f6a8f2302f39e10819d429e257890c8bb2ae4b4f
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-04-07 19:25:56 +0300

    Add modals instead of window.confirm

--- frontend/src/pages/Admin/ProductUpdate.jsx
+++ frontend/src/pages/Admin/ProductUpdate.jsx
@@ -14,6 +14,7 @@ import { useFetchCategoriesQuery } from "../../redux/api/categoryApiSlice";
 import AdminMenu from "./AdminMenu";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
+import Modal from "../../components/Modal";
 
 const AdminProductUpdate = () => {
   const params = useParams();
@@ -48,6 +49,8 @@ const AdminProductUpdate = () => {
 
   // Define the delete product mutation
   const [deleteProduct] = useDeleteProductMutation();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     if (productData && productData._id) {
@@ -116,26 +119,29 @@ const AdminProductUpdate = () => {
     }
   };
 
-  const handleDelete = async () => {
-    try {
-      let answer = window.confirm(
-        "Are you sure you want to delete this product?"
-      );
-      if (!answer) return;
 
+  const confirmDelete = async () => {
+    try {
       const { data } = await deleteProduct(params._id);
       toast.success(`"${data.name}" is deleted`, {
         position: toast.POSITION.TOP_RIGHT,
         autoClose: 2000,
       });
       navigate("/admin/allproductslist");
+      setIsModalOpen(false);
     } catch (err) {
-      console.log(err);
+      console.error(err);
       toast.error("Delete failed. Try again.", {
         position: toast.POSITION.TOP_RIGHT,
         autoClose: 2000,
       });
+      
+      setIsModalOpen(false);
     }
+  };
+
+  const handleDeleteClick = () => {
+    setIsModalOpen(true);
   };
 
   return (
@@ -144,7 +150,7 @@ const AdminProductUpdate = () => {
         <div className="flex flex-col md:flex-row">
           <AdminMenu />
           <div className="md:w-3/4 p-3">
-            <div className="h-12 text-center">{t('update_delete_product')}</div>
+            <div className="h-12 text-center">{t("update_delete_product")}</div>
 
             {image && (
               <div className="text-center mx-auto max-w-xs md:max-w-md">
@@ -169,101 +175,117 @@ const AdminProductUpdate = () => {
               </label>
             </div>
             <div className="flex flex-col justify-center md:ml-[8rem]">
-            <div className="p-3">
-              <div className="flex flex-wrap">
-                <div className="one">
-                  <label htmlFor="name">  {t('name')}</label> <br />
-                  <Input
-                    type="text"
-                    className="p-4 mb-3 w-[30rem] border rounded-lg bg-white text-blac dark:bg-[#3A3A3A] dark:text-white mr-[5rem]"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
+              <div className="p-3">
+                <div className="flex flex-wrap">
+                  <div className="one">
+                    <label htmlFor="name"> {t("name")}</label> <br />
+                    <Input
+                      type="text"
+                      className="p-4 mb-3 w-[30rem] border rounded-lg bg-white text-blac dark:bg-[#3A3A3A] dark:text-white mr-[5rem]"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="two">
+                    <label htmlFor="name block"> {t("price")}</label> <br />
+                    <Input
+                      type="number"
+                      className="p-4 mb-3 w-[30rem] border rounded-lg bg-white text-black dark:bg-[#3A3A3A] dark:text-white "
+                      value={price}
+                      onChange={(e) => setPrice(e.target.value)}
+                    />
+                  </div>
                 </div>
 
-                <div className="two">
-                  <label htmlFor="name block">  {t('price')}</label> <br />
-                  <Input
-                    type="number"
-                    className="p-4 mb-3 w-[30rem] border rounded-lg bg-white text-black dark:bg-[#3A3A3A] dark:text-white "
-                    value={price}
-                    onChange={(e) => setPrice(e.target.value)}
-                  />
-                </div>
-              </div>
-
-              <div className="flex flex-wrap">
-                <div>
-                  <label htmlFor="name block">  {t('quantity')}</label> <br />
-                  <Input
-                    type="number"
-                    min="1"
-                    className="p-4 mb-3 w-[30rem] border rounded-lg bg-white text-black dark:bg-[#3A3A3A] dark:text-white mr-[5rem]"
-                    value={quantity}
-                    onChange={(e) => setQuantity(e.target.value)}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="name block">  {t('brand')}</label> <br />
-                  <Input
-                    type="text"
-                    className="p-4 mb-3 w-[30rem] border rounded-lg bg-white text-black dark:bg-[#3A3A3A] dark:text-white"
-                    value={brand}
-                    onChange={(e) => setBrand(e.target.value)}
-                  />
-                </div>
-              </div>
-
-              <label htmlFor="" className="my-5">
-              {t('description')}
-              </label>
-              <textarea
-                type="text"
-                className="p-2 mb-3  border rounded-lg w-[95%] bg-white text-black dark:bg-[#3A3A3A] dark:text-white"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-              />
-
-              <div className="flex flex-col justify-between">
-                <div>
-                  <label htmlFor="name block">  {t('count_in_stock')}</label> <br />
-                  <Input
-                    type="text"
-                    className="p-4 mb-3 w-[30rem] border rounded-lg bg-white text-black dark:bg-[#3A3A3A] dark:text-white "
-                    value={stock}
-                    onChange={(e) => setStock(e.target.value)}
-                  />
+                <div className="flex flex-wrap">
+                  <div>
+                    <label htmlFor="name block"> {t("quantity")}</label> <br />
+                    <Input
+                      type="number"
+                      min="1"
+                      className="p-4 mb-3 w-[30rem] border rounded-lg bg-white text-black dark:bg-[#3A3A3A] dark:text-white mr-[5rem]"
+                      value={quantity}
+                      onChange={(e) => setQuantity(e.target.value)}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="name block"> {t("brand")}</label> <br />
+                    <Input
+                      type="text"
+                      className="p-4 mb-3 w-[30rem] border rounded-lg bg-white text-black dark:bg-[#3A3A3A] dark:text-white"
+                      value={brand}
+                      onChange={(e) => setBrand(e.target.value)}
+                    />
+                  </div>
                 </div>
 
-                <div>
-                  <label htmlFor="">  {t('category')}</label> <br />
-                  <select
-                    placeholder="Choose Category"
-                    className="p-4 mb-3 w-[30rem] border rounded-lg bg-white text-black dark:bg-[#3A3A3A] dark:text-white mr-[5rem]"
-                    onChange={(e) => setCategory(e.target.value)}
-                  >
-                    {categories?.map((c) => (
-                      <option key={c._id} value={c._id}>
-                        {c.name}
-                      </option>
-                    ))}
-                  </select>
+                <label htmlFor="" className="my-5">
+                  {t("description")}
+                </label>
+                <textarea
+                  type="text"
+                  className="p-2 mb-3  border rounded-lg w-[95%] bg-white text-black dark:bg-[#3A3A3A] dark:text-white"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+
+                <div className="flex flex-col justify-between">
+                  <div>
+                    <label htmlFor="name block"> {t("count_in_stock")}</label>{" "}
+                    <br />
+                    <Input
+                      type="text"
+                      className="p-4 mb-3 w-[30rem] border rounded-lg bg-white text-black dark:bg-[#3A3A3A] dark:text-white "
+                      value={stock}
+                      onChange={(e) => setStock(e.target.value)}
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor=""> {t("category")}</label> <br />
+                    <select
+                      placeholder="Choose Category"
+                      className="p-4 mb-3 w-[30rem] border rounded-lg bg-white text-black dark:bg-[#3A3A3A] dark:text-white mr-[5rem]"
+                      onChange={(e) => setCategory(e.target.value)}
+                    >
+                      {categories?.map((c) => (
+                        <option key={c._id} value={c._id}>
+                          {c.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
                 </div>
-              </div>
               </div>
               <div className="">
                 <Button
                   onClick={handleSubmit}
                   className="py-4 px-10 mt-5 rounded-lg text-lg font-bold  bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800  mr-6"
                 >
-                    {t('update')}
+                  {t("update")}
                 </Button>
                 <Button
-                  onClick={handleDelete}
+                  onClick={handleDeleteClick}
                   className="py-4 px-10 mt-5 rounded-lg text-lg font-bold  bg-red-600"
                 >
-                    {t('delete')}
+                  {t("delete")}
                 </Button>
+
+                <Modal
+                  isOpen={isModalOpen}
+                  onClose={() => setIsModalOpen(false)}
+                >
+                  <p className="text-black">Are you sure you want to delete this product? </p>
+                  <div className="flex justify-between">
+                  <Button onClick={confirmDelete} className="py-2 px-5 mt-5 rounded-lg text-lg font-bold  bg-red-600">
+                   Confirm
+                  </Button>
+                  <Button onClick={() => setIsModalOpen(false)} className="p2-4 px-5 mt-5 rounded-lg text-lg font-bold  bg-gray-600">
+                    Cancel
+                    </Button>
+                    </div>
+                </Modal>
               </div>
             </div>
           </div>
--- frontend/src/pages/Admin/UserList.jsx
+++ frontend/src/pages/Admin/UserList.jsx
@@ -12,9 +12,8 @@ import {
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
 import AdminMenu from "./AdminMenu";
-import Metadata from "../../components/Metadata";
 import Button from "../../components/Button";
-import Input from "../../components/Input";
+import Modal from "../../components/Modal";
 
 const UserList = () => {
   const { data: users, refetch, isLoading, error } = useGetUsersQuery();
@@ -29,17 +28,34 @@ const UserList = () => {
 
   const { t } = useTranslation();
 
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [userToDelete, setUserToDelete] = useState(null);
+
+
+
+
   useEffect(() => {
     refetch();
   }, [refetch]);
 
-  const deleteHandler = async (id) => {
-    if (window.confirm("Are you sure you want to delete this user?")) {
+
+  const deleteHandler = (user) => {
+    setUserToDelete(user);
+    setIsDeleteModalOpen(true);
+  };
+
+  const confirmDelete = async () => {
+    if (userToDelete) {
       try {
-        await deleteUser(id);
-        refetch();
+        await deleteUser(userToDelete._id).unwrap();
+        toast.success("User deleted successfully");
+        refetch(); 
+        setIsDeleteModalOpen(false);
+        setUserToDelete(null);
       } catch (err) {
         toast.error(err?.data?.message || err.error);
+        setIsDeleteModalOpen(false);
+        setUserToDelete(null);
       }
     }
   };
@@ -66,89 +82,110 @@ const UserList = () => {
 
   return (
     <div className="p-4 my-12 md:ml-[10rem]">
-      <h1 className="text-2xl font-semibold my-4 text-black dark:text-slate-50 text-center">{t('users')}</h1>
+      <h1 className="text-2xl font-semibold my-4 text-black dark:text-slate-50 text-center">
+        {t("users")}
+      </h1>
       {isLoading ? (
         <Loader />
       ) : error ? (
-        <Message variant="danger">{t('error_message')}</Message>
-        ) : (
-            <div>
-               <AdminMenu />
-        <div className="overflow-x-auto">
-          <table className="min-w-full">
-            <thead>
-              <tr className="bg-gray-100 dark:bg-gray-700">
-                <th className="px-4 py-2 text-left">{t('name')}</th>
-                <th className="px-4 py-2 text-left">{t('email')}</th>
-                <th className="px-4 py-2 text-left">{t('admin')}</th>
-              </tr>
-            </thead>
-            <tbody>
-    {users.map((user) => (
-      <tr key={user._id} className="border-b dark:border-gray-600">
-        <td className="px-4 py-2">
-          {editableUserId === user._id ? (
-            <input
-              type="text"
-              value={editableUserName}
-              onChange={(e) => setEditableUserName(e.target.value)}
-              className="w-full p-2 border rounded-lg"
-            />
-          ) : (
-            <span>{user.username}</span>
-          )}
-        </td>
-        <td className="px-4 py-2">
-          {editableUserId === user._id ? (
-            <input
-              type="email"
-              value={editableUserEmail}
-              onChange={(e) => setEditableUserEmail(e.target.value)}
-              className="w-full p-2 border rounded-lg"
-            />
-          ) : (
-            <a href={`mailto:${user.email}`}>{user.email}</a>
-          )}
-        </td>
-        <td className="px-4 py-2">
-          {user.isAdmin ? (
-            <FaCheck className="text-green-500" />
-          ) : (
-            <FaTimes className="text-red-500" />
-          )}
-        </td>
-        <td className="px-4 py-2 flex items-center">
-          {editableUserId === user._id ? (
-            <>
-              <Button onClick={() => updateHandler(user._id)}>
-                <FaCheck className="text-green-500" />
-              </Button>
-              <Button onClick={() => setEditableUserId(null)}>
-                <FaTimes className="text-red-500" />
-              </Button>
-            </>
-          ) : (
-            <>
-              <Button className="text-blue-600 px-2" onClick={() => toggleEdit(user._id, user.username, user.email)}>
-                <FaEdit />
-              </Button>
-              <Button className="text-red-600" onClick={() => deleteHandler(user._id)}>
-                <FaTrash />
-              </Button>
-            </>
-          )}
-        </td>
-      </tr>
-    ))}
-  </tbody>
-          </table>
-              </div>
-              </div>
+        <Message variant="danger">{t("error_message")}</Message>
+      ) : (
+        <div>
+          <AdminMenu />
+          <div className="overflow-x-auto">
+            <table className="min-w-full">
+              <thead>
+                <tr className="bg-gray-100 dark:bg-gray-700">
+                  <th className="px-4 py-2 text-left">{t("name")}</th>
+                  <th className="px-4 py-2 text-left">{t("email")}</th>
+                  <th className="px-4 py-2 text-left">{t("admin")}</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user) => (
+                  <tr key={user._id} className="border-b dark:border-gray-600">
+                    <td className="px-4 py-2">
+                      {editableUserId === user._id ? (
+                        <input
+                          type="text"
+                          value={editableUserName}
+                          onChange={(e) => setEditableUserName(e.target.value)}
+                          className="w-full p-2 border rounded-lg"
+                        />
+                      ) : (
+                        <span>{user.username}</span>
+                      )}
+                    </td>
+                    <td className="px-4 py-2">
+                      {editableUserId === user._id ? (
+                        <input
+                          type="email"
+                          value={editableUserEmail}
+                          onChange={(e) => setEditableUserEmail(e.target.value)}
+                          className="w-full p-2 border rounded-lg"
+                        />
+                      ) : (
+                        <a href={`mailto:${user.email}`}>{user.email}</a>
+                      )}
+                    </td>
+                    <td className="px-4 py-2">
+                      {user.isAdmin ? (
+                        <FaCheck className="text-green-500" />
+                      ) : (
+                        <FaTimes className="text-red-500" />
+                      )}
+                    </td>
+                    <td className="px-4 py-2 flex items-center">
+                      {editableUserId === user._id ? (
+                        <>
+                          <Button onClick={() => updateHandler(user._id)}>
+                            <FaCheck className="text-green-500" />
+                          </Button>
+                          <Button onClick={() => setEditableUserId(null)}>
+                            <FaTimes className="text-red-500" />
+                          </Button>
+                        </>
+                      ) : (
+                        <>
+                          <Button
+                            className="text-blue-600 px-2"
+                            onClick={() =>
+                              toggleEdit(user._id, user.username, user.email)
+                            }
+                          >
+                            <FaEdit />
+                          </Button>
+                          <Button
+                            className="text-red-600"
+                            onClick={() => deleteHandler(user)}
+                          >
+                            <FaTrash />
+                          </Button>
+                          <Modal
+                            isOpen={isDeleteModalOpen}
+                            onClose={() => setIsDeleteModalOpen(false)}
+                          >
+                              <p className="text-black">Are you sure you want to delete this user?</p>
+                              <div className="flex justify-between">
+                            <Button onClick={confirmDelete} className="py-2 px-5 mt-5 rounded-lg text-lg font-bold  bg-red-600">
+                             Confirm
+                            </Button>
+                            <Button onClick={() => setIsDeleteModalOpen(false)} className="py-2 px-5 mt-5 rounded-lg text-lg font-bold  bg-gray-600">
+                              Cancel
+                                </Button>
+                                </div>
+                          </Modal>
+                        </>
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       )}
     </div>
-
-
-
   );
 };
 
