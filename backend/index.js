6ab40901799a3646b39caa60191140b66f80055a
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-04-19 16:27:46 +0300

    Fix cors

--- backend/index.js
+++ backend/index.js
@@ -29,8 +29,13 @@ app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const corsOptions = {
+  origin: 'https://main--eshop28.netlify.app/', 
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 
 app.use("/api/users", userRoutes);
