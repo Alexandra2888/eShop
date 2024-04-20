828ff9e35deb99333dc84403e7807fc1d41d80f5
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-04-20 22:31:57 +0300

    Add credentials in api slice

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,32 +1,12 @@
-// import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
-// import { BASE_URL } from "../constants";
-
-// const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
-
-// export const apiSlice = createApi({
-//   baseQuery,
-//   tagTypes: ["Product", "Order", "User", "Category"],
-//   endpoints: () => ({}),
-// });
-
-
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants";
 
-const baseQuery = fetchBaseQuery({
-  baseUrl: BASE_URL,
-  prepareHeaders: (headers, { getState }) => {
-    const token = getState().auth.token(); 
-    if (token) {
-      headers.set('authorization', `Bearer ${token}`);
-    }
-    return headers;
-  },
-  credentials: 'include', 
-});
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL, withCredentials:true });
 
 export const apiSlice = createApi({
   baseQuery,
   tagTypes: ["Product", "Order", "User", "Category"],
   endpoints: () => ({}),
 });
+
+
