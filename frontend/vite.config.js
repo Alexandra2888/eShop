7cd98083a3739fe03d76914b122ea62feb1f027e
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-04-21 23:07:11 +0300

    url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,26 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "https://e-shop-rust-five.vercel.app/",
-      "/uploads/": "https://e-shop-rust-five.vercel.app/",
+      // "/api/": "https://e-shop-rust-five.vercel.app/",
+      // "/uploads/": "https://e-shop-rust-five.vercel.app/",
 
       
       // "/api/": "http://localhost:5000",
       // "/uploads/": "http://localhost:5000",
+
+      
+        '/api/': {
+          target: 'e-shop-rust-five.vercel.app', 
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      '/uploads/': {
+        target: 'e-shop-rust-five.vercel.app', 
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/uploads/, '')
+  }
     },
    
     },
