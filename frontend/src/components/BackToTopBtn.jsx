4c5ac17fe6a673083c3afc2bb9cc923f036a384a
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-04-22 18:42:48 +0300

    Fix es lint. format files.

--- frontend/src/components/BackToTopBtn.jsx
+++ frontend/src/components/BackToTopBtn.jsx
@@ -1,4 +1,4 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const TopBtn = () => {
   const [isScrollButtonVisible, setIsScrollButtonVisible] = useState(false);
@@ -12,34 +12,36 @@ const TopBtn = () => {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', checkScrollHeight);
+    window.addEventListener("scroll", checkScrollHeight);
     return () => {
-      window.removeEventListener('scroll', checkScrollHeight);
+      window.removeEventListener("scroll", checkScrollHeight);
     };
   }, []);
 
   const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
     <div
-    className={`fixed bottom-8 right-8 items-center h-10 w-10 justify-center z-40 cursor-pointer bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800 rounded-full transition-transform duration-300 scale-100 hover:scale-110 ${isScrollButtonVisible ? 'flex' : 'hidden'}`}
-    onClick={scrollToTop}
-  >
-    <svg
-      width={24}
-      height={24}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="#131212"
-      strokeWidth={3}
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      className={`fixed bottom-8 right-8 items-center h-10 w-10 justify-center z-40 cursor-pointer bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800 rounded-full transition-transform duration-300 scale-100 hover:scale-110 ${
+        isScrollButtonVisible ? "flex" : "hidden"
+      }`}
+      onClick={scrollToTop}
     >
-      <path d="M12 19V5M5 12l7-7 7 7" />
-    </svg>
-  </div>
+      <svg
+        width={24}
+        height={24}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="#131212"
+        strokeWidth={3}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M12 19V5M5 12l7-7 7 7" />
+      </svg>
+    </div>
   );
 };
 
