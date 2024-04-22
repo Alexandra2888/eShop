92886d197f01b0e37adc9ac892a51d46eb12c5c4
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-04-22 19:05:57 +0300

    Fix copyright sign

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -170,7 +170,7 @@ const Footer = () => {
       </div>
       <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24">
           <p className="text-xs leading-5  text-center ">
-          {t("copyright")}
+          &copy; {t("copyright")}
             </p>
       </div>
     </div>
