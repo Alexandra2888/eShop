8a4a741f1c3ac4d95a9059f4e6de3f6622dfb3e2
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-04-22 12:10:02 +0300

    Revert to localhost

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://e-shop-rust-five.vercel.app/";
+export const BASE_URL = "";
 
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,26 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // "/api/": "https://e-shop-rust-five.vercel.app/",
-      // "/uploads/": "https://e-shop-rust-five.vercel.app/",
-
-      
-      // "/api/": "http://localhost:5000",
-      // "/uploads/": "http://localhost:5000",
-
-      
-        '/api/': {
-          target: 'e-shop-rust-five.vercel.app', 
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/api/, '')
-      },
-      '/uploads/': {
-        target: 'e-shop-rust-five.vercel.app', 
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/uploads/, '')
-  }
+       "/api/": "http://localhost:5000",
+      "/uploads/": "http://localhost:5000",
     },
    
     },
