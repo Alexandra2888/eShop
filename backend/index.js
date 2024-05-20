af91d87c4be450b32acd6012019313dd89858f04
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-05-20 20:18:16 +0300

    corsoptions

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: 'https://main--eshop28.netlify.app',
+  origin: 'https://eshop-1-xj6m.onrender.com/',
 };
 
 app.use(cors(corsOptions));
