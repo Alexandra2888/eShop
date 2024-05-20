00ae93cc5008b87cf7a188a60448dfbbcb802b00
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-05-20 19:37:20 +0300

    index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-// packages
+// Import necessary packages
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 
-// Utiles
+// Import utility functions and routes
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
@@ -16,6 +16,7 @@ import orderRoutes from "./routes/orderRoutes.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
 
+// Connect to database
 connectDB();
 
 const app = express();
@@ -24,14 +25,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
-
 const corsOptions = {
   origin: 'https://main--eshop28.netlify.app',
-}
+};
 
 app.use(cors(corsOptions));
-
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -49,7 +47,16 @@ app.get("/api/config/paypal", (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 
+// Serve static files from the React frontend app
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+}
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
