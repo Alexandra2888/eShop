8f1983f51f763e955531d4a3150c1fb3f726aed8
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-05-20 14:42:04 +0300

    Update base_url for render

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://e-shop-4bls.vercel.app/";
+export const BASE_URL = "https://eshop-yn0p.onrender.com";
 
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
