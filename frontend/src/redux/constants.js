b202d34cc36a7d6a2f4cafac3d6a74be9da37ab9
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-05-20 20:37:48 +0300

    revert base_url

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://eshop-1-xj6m.onrender.com";
+export const BASE_URL = "http://localhost:5000/";
 
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
