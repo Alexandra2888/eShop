8df3b6ad6f8fefad6e23d4712c88c7d16a0f8f5a
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-10-20 16:51:31 +0300

    Update the Build Command

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
     proxy: {
-        "/api/": "http://localhost:5000",
+      "/api/": "http://localhost:5000",
       "/uploads/": "http://localhost:5000",
-   }
     },
-
+  },
 });
